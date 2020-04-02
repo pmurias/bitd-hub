f279a8b7ad214b04644e711fa74cc5eb9cc142eb
Paweł Murias <pawelmurias@gmail.com>
2020-04-02 12:19:09 +0200

    Make checkboxes a form component

--- src/app/checkboxes/checkboxes.component.ts
+++ src/app/checkboxes/checkboxes.component.ts
@@ -1,13 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-checkboxes',
   templateUrl: './checkboxes.component.html',
   styleUrls: ['./checkboxes.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => CheckboxesComponent),
+      multi: true
+    }
+  ]
 })
-export class CheckboxesComponent implements OnInit {
+export class CheckboxesComponent implements OnInit, ControlValueAccessor {
   ticked : number;
   boxes : any;
+
+  onChange = (_: number) => {};
 
   constructor() {
     this.ticked = 0;
@@ -20,11 +30,25 @@ export class CheckboxesComponent implements OnInit {
   ngOnInit(): void {}
 
   check(index, value) {
-    console.log('check', index, value);
     this.ticked = value ? index + 1 : index;
+    this.setBoxes();
+    this.onChange(this.ticked);
+  }
 
+  setBoxes() {
     for (let i = 0; i < this.boxes.length; i++) {
       this.boxes[i].value = i < this.ticked;
     }
+  }
+
+  registerOnChange(onChange) {
+    this.onChange = onChange;
+  }
+
+  registerOnTouched() {}
+
+  writeValue(value) {
+    this.ticked = value;
+    this.setBoxes();
   }
 }
